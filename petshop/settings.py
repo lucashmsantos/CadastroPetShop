c20a448020d6676ba165632465910a87292406d6
Lucas Henrique Marques dos Santos <lucash_marques@hotmail.com>
2022-05-18 20:13:29 -0300

    deploy

--- petshop/settings.py
+++ petshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-r^5a3#ne%1mokp1f51qwvs(3$r0c-@wi)+u3xbbnm=3#sfpvhx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://cadastropetshop.herokuapp.com/']
 
 
 # Application definition
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
